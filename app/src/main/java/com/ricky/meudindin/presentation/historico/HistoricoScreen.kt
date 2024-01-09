1f440f7d48735af3ae7553e44dae6938732958bc
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2024-01-09 10:34:41 -0300

    recuperar historico com base no filtro

--- app/src/main/java/com/ricky/meudindin/presentation/historico/HistoricoScreen.kt
+++ app/src/main/java/com/ricky/meudindin/presentation/historico/HistoricoScreen.kt
@@ -16,32 +16,32 @@ fun HistoricoScreen(state: HistoricoState, onEvent: (HistoricoEvent) -> Unit) {
     FlowRow {
         BtnText(
             label = R.string.sete_dias,
-            onClick = { onEvent(HistoricoEvent.OnChangeFiltro(7)) },
+            onClick = { onEvent(HistoricoEvent.OnChangeFiltro("7_dias")) },
             isPress = state.filtro == "7_dias"
         )
         BtnText(
             label = R.string.cartorze_dias,
-            onClick = { onEvent(HistoricoEvent.OnChangeFiltro(7)) },
+            onClick = { onEvent(HistoricoEvent.OnChangeFiltro("14_dias")) },
             isPress = state.filtro == "14_dias"
         )
         BtnText(
             label = R.string.um_mes,
-            onClick = { onEvent(HistoricoEvent.OnChangeFiltro(7)) },
+            onClick = { onEvent(HistoricoEvent.OnChangeFiltro("1_mes")) },
             isPress = state.filtro == "1_mes"
         )
         BtnText(
             label = R.string.seis_meses,
-            onClick = { onEvent(HistoricoEvent.OnChangeFiltro(7)) },
+            onClick = { onEvent(HistoricoEvent.OnChangeFiltro("6_meses")) },
             isPress = state.filtro == "6_meses"
         )
         BtnText(
             label = R.string.um_ano,
-            onClick = { onEvent(HistoricoEvent.OnChangeFiltro(7)) },
+            onClick = { onEvent(HistoricoEvent.OnChangeFiltro("1_ano")) },
             isPress = state.filtro == "1_ano"
         )
         BtnText(
             label = R.string.total,
-            onClick = { onEvent(HistoricoEvent.OnChangeFiltro(7)) },
+            onClick = { onEvent(HistoricoEvent.OnChangeFiltro("total")) },
             isPress = state.filtro == "total"
         )
     }
--- app/src/main/java/com/ricky/meudindin/presentation/historico/HistoricoViewModel.kt
+++ app/src/main/java/com/ricky/meudindin/presentation/historico/HistoricoViewModel.kt
@@ -2,10 +2,13 @@ package com.ricky.meudindin.presentation.historico
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ricky.meudindin.common.calculateDateAgo
 import com.ricky.meudindin.domain.repository.FinancaRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.util.Calendar
 import javax.inject.Inject
@@ -24,12 +27,58 @@ class HistoricoViewModel @Inject constructor(private val financaRepository: Fina
 
     private fun recuperaFinancas() {
         viewModelScope.launch {
+            val startDate: Long
+            val endDate: Long
+
             when (_state.value.filtro) {
                 "7_dias" -> {
-
+                    endDate = calculateDateAgo(7, Calendar.DAY_OF_YEAR)
+                    startDate = calendar.timeInMillis
                 }
+
+                "14_dias" -> {
+                    endDate = calculateDateAgo(14, Calendar.DAY_OF_YEAR)
+                    startDate = calendar.timeInMillis
+                }
+
+                "1_mes" -> {
+                    endDate = calculateDateAgo(1, Calendar.MONTH)
+                    startDate = calendar.timeInMillis
+                }
+
+                "6_meses" -> {
+                    endDate = calculateDateAgo(6, Calendar.MONTH)
+                    startDate = calendar.timeInMillis
+                }
+
+                "1_ano" -> {
+                    endDate = calculateDateAgo(1, Calendar.YEAR)
+                    startDate = calendar.timeInMillis
+                }
+
+                "total" -> {
+                    financaRepository.getAllFinanca().collect { financasRecuperadas ->
+                        _state.update {
+                            it.copy(
+                                financas = financasRecuperadas
+                            )
+                        }
+                    }
+                    return@launch
+                }
+                else -> return@launch
             }
 
+            financaRepository.getFinancaByDate(
+                startDate = startDate,
+                endDate = endDate
+            ).collect { financasRecuperadas ->
+                _state.update {
+                    it.copy(
+                        financas = financasRecuperadas
+                    )
+                }
+            }
         }
     }
 }

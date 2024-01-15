6a6a6b917249e037bc24afc143fb748f19887dfe
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2024-01-15 10:53:52 -0300

    despesa item compose

---
+++ app/src/main/java/com/ricky/meudindin/presentation/historico/components/DespesaItem.kt
@@ -0,0 +1,103 @@
+package com.ricky.meudindin.presentation.historico.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ricky.meudindin.domain.enums.TipoDespesa
+import com.ricky.meudindin.domain.model.Financa
+import com.ricky.meudindin.ui.theme.MeuDinDInTheme
+
+@Composable
+fun DespesaItem(
+    modifier: Modifier = Modifier,
+    financa: Financa
+) {
+    Surface(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = financa.tipo!!.value,
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            Box {
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .size(48.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Divider(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .width(4.dp)
+                    )
+                }
+                Column(
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Surface(
+                        modifier = Modifier.size(48.dp),
+                        color = MaterialTheme.colorScheme.primaryContainer,
+                        shape = CircleShape,
+                    ) {
+                        Icon(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(8.dp)
+                                .clip(CircleShape)
+                                .background(MaterialTheme.colorScheme.primaryContainer),
+                            imageVector = financa.tipo!!.icon,
+                            contentDescription = financa.tipo!!.value
+                        )
+                    }
+                }
+            }
+            Text(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                text = financa.saida.toString(),
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DespesaItem() {
+    MeuDinDInTheme {
+        DespesaItem(financa = Financa(tipo = TipoDespesa.AGUA))
+    }
+}
